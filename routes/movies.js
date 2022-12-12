337124d9648573a66f582603a91d84c1363f9e8e
Abram Gallegos <abramig2@illinois.edu>
2022-12-12 01:29:43 -0600

    Fixed movies get

--- routes/movies.js
+++ routes/movies.js
@@ -4,40 +4,10 @@ var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
 
-router.get('/', function (req, res) {
-    const conditions = req.query.where ? JSON.parse(req.query.where) : {};
-    const projections = req.query.select ? JSON.parse(req.query.select) : (req.query.filter ? JSON.parse(req.query.filter) : {});
-    const options = {}
-    options.skip = req.query.skip ? JSON.parse(req.query.skip) : 0;
-    options.limit = req.query.limit ? JSON.parse(req.query.limit) : 0;
-    options.sort = req.query.sort ? JSON.parse(req.query.sort) : {};
-
-    movieModel.find(conditions, projections, options, function (err, results) {
-        if (err) {
-            res.status(500).send({
-                message: 'Internal Server error',
-                data: {}
-            })
-        } else {
-            if (req.query.count && req.query.count === true) {
-                res.status(200).send({
-                    message: 'OK',
-                    data: results.length
-                })
-            } else {
-                res.status(200).send({
-                    message: 'OK',
-                    data: results
-                })
-            }
-        }
-    })
-})
-
 router.post('/', function (req, res) {
     if (req.body.genre_ids && req.body.id && req.body.original_language && req.body.overview && req.body.poster_path && req.body.release_date && req.body.title && req.body.vote_average) {
-        movieModel.find({name: req.body.id}, function (err, result) {
-            if (result.length) {
+        movieModel.findOne({id: req.body.id}, function (err, result) {
+            if (result) {
                 res.status(404).send({
                     message: 'Movie already exists in db',
                     data: result
@@ -82,22 +52,6 @@ router.post('/', function (req, res) {
 })
 
 router.get('/:id', function (req, res) {
-    movieModel.findOne({_id: req.params.id}, function (err, result) {
-        if (err) {
-            res.status(404).send({
-                message: 'Movie Not Found',
-                data: {}
-            })
-        } else {
-            res.status(200).send({
-                message: 'OK',
-                data: result
-            })
-        }
-    })
-})
-
-router.get('/:id', function (req, res) {
     movieModel.findOne({id: req.params.id}, function (err, result) {
         if (err) {
             res.status(404).send({
@@ -108,31 +62,6 @@ router.get('/:id', function (req, res) {
             res.status(200).send({
                 message: 'OK',
                 data: result
-            })
-        }
-    })
-})
-
-router.delete('/:id', function (req, res) {
-    movieModel.findByIdAndDelete(req.params.id, function (err) {
-        if (err) {
-            res.status(404).send({
-                message: 'Unsuccessfully deleted Movie',
-                data: {}
-            })
-        } else {
-            userModel.updateMany({movieList: {$elemMatch: {$eq: req.params.id}}}, {$pullAll: {movieList: [req.params.id]}}, function (err, result) {
-                if (err) {
-                    res.status(404).send({
-                        message: 'Unsuccessfully deleted Movie',
-                        data: {}
-                    })
-                } else {
-                    res.status(200).send({
-                        message: 'Successfully deleted Movie',
-                        data: result
-                    })
-                }
             })
         }
     })
