5fdbf090a1c8e0e6fb1334792e15509f95e7c2c0
aigallegos <39970498+aigallegos@users.noreply.github.com>
2023-01-16 19:10:30 -0600

    Delete secrets.js

--- config/secrets.js
+++
@@ -1,8 +0,0 @@
-/*
- * You generally want to .gitignore this file to prevent important credentials from being stored on your public repo.
- */
-module.exports = {
-    token : "secret-starter-mern",
-    mongo_connection : "mongodb+srv://abramig2:Password@finalproj.bnycf2v.mongodb.net/?retryWrites=true&w=majority"
-    //example: mongo_connection : "mongodb+srv://[type-yours]:[type-yours]@[type-yours-web-provided].mongodb.net/test?retryWrites=true"
-};
